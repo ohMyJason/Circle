109a752de5a72ad9550296292608d8835b789be3
ohmyjason <719134834@qq.com>
2019-09-07 16:03:36 +0800

    first commit

---
+++ src/main/java/com/lanqiao/circle/CircleApplication.java
@@ -0,0 +1,13 @@
+package com.lanqiao.circle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleApplication.class, args);
+    }
+
+}
