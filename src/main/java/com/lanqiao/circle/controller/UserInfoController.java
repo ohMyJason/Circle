7b4060c8bc7b4cd56bb2b13f41a98bd15b794ca9
C-H-Han <admin@honkerac.cn>
2019-09-10 19:06:24 +0800

    细化查询粉丝，关注  自己查询按token 别人查询按userId

--- src/main/java/com/lanqiao/circle/controller/UserInfoController.java
+++ src/main/java/com/lanqiao/circle/controller/UserInfoController.java
@@ -1,7 +1,9 @@
 package com.lanqiao.circle.controller;
 
 
+import com.lanqiao.circle.annotations.UserLoginToken;
 import com.lanqiao.circle.entity.Users;
+import com.lanqiao.circle.service.TokenService;
 import com.lanqiao.circle.service.UserInfoService;
 import com.lanqiao.circle.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,49 +12,81 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/userInfo")
 public class UserInfoController {
     @Autowired
     UserInfoService userInfoService;
+    @Autowired
+    TokenService tokenService;
+
 
     /**
      * 查询用户基本信息
-     * @Param  userId
+     * @Param   头部加token
      * @return  Users
      */
+    @UserLoginToken
     @PostMapping("getUserInfo")
-    public Result getUserInfoByUserId(@RequestParam(name = "userId") int userId){
+    public Result getUserInfoByUserId(HttpServletRequest httpServletRequest){
+        int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
         return userInfoService.getUserInfoByUserId(userId);
     }
 
     /**
      * 修改用户基本信息
-     * @Param  Users
+     * @Param  头部加token, Users
      * @return  int
      */
+    @UserLoginToken
     @PostMapping("updateUserInfo")
-    public Result updateUserInfo(Users users){
+    public Result updateUserInfo(HttpServletRequest httpServletRequest,Users users){
+        int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
+        users.setUserId(userId);
         return userInfoService.updateUserInfo(users);
     }
 
     /**
-     * 查询用户粉丝
+     * 自己查询粉丝
+     * @Param  头部加token
+     * @return  FansList
+     */
+    @UserLoginToken
+    @PostMapping("getAllFansSelf")
+    public Result getAllFansToken(HttpServletRequest httpServletRequest){
+        int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
+        return userInfoService.getFansByUserId(userId);
+    }
+    /**
+     * 查询别人的粉丝
      * @Param  userId
      * @return  FansList
      */
-    @PostMapping("getAllFans")
-    public Result getAllFansByUserId(@RequestParam(name = "userId") int userId){
+    @PostMapping("getAllFansOthers")
+    public Result getAllFansByUserId(@RequestParam(name = "userId")int userId){
         return userInfoService.getFansByUserId(userId);
+    }
+    /**
+     * 自己查询关注
+     * @Parm  头部加token
+     * @return  BloggerList
+     */
+    @UserLoginToken
+    @PostMapping("getAllBloggerSelf")
+    public Result getAllBloggerByToken(HttpServletRequest httpServletRequest){
+        int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
+        return userInfoService.getBloggerByUserId(userId);
     }
 
     /**
-     * 查询用户关注
-     * @Parm  userId
+     * 查询别人的关注
+     * @Parm  头部加token
      * @return  BloggerList
      */
-    @PostMapping("getAllBlogger")
-    public Result getAllBloggerByUserId(@RequestParam(name = "userId") int userId){
+    @PostMapping("getAllBloggerOthers")
+    public Result getAllBloggerByUserId(@RequestParam(name = "userId")int userId){
         return userInfoService.getBloggerByUserId(userId);
     }
 
