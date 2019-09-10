3b1bb995a057bd312a4a823499fd653c4fc66890
ohmyjason <719134834@qq.com>
2019-09-10 16:11:35 +0800

    添加：null 删除：null 修改：改了一下前端目录 修复bug：null

--- src/main/java/com/lanqiao/circle/controller/LoginController.java
+++ src/main/java/com/lanqiao/circle/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.lanqiao.circle.controller;
 
+import com.lanqiao.circle.mapper.UsersMapper;
 import com.lanqiao.circle.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/userLogin")
 public class LoginController {
 
+
+    @Autowired
+    UsersMapper usersMapper;
+
+
+    /**
+     * 登录 可选择手机号登录，用户名登录，邮箱登录
+     * @param loginName
+     * @param password
+     * @return
+     */
     @PostMapping("/login")
-    public Result login(){
+    public Result login(String loginName,String password){
+
 
         return  null;
     }
