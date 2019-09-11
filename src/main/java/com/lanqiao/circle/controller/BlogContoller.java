7a33c59ea40096a8fcdcb645801698cbfeff31a7
C-H-Han <admin@honkerac.cn>
2019-09-11 19:43:00 +0800

    实现转发功能

--- src/main/java/com/lanqiao/circle/controller/BlogContoller.java
+++ src/main/java/com/lanqiao/circle/controller/BlogContoller.java
@@ -1,8 +1,10 @@
 package com.lanqiao.circle.controller;
 
 import com.lanqiao.circle.annotations.UserLoginToken;
+import com.lanqiao.circle.entity.Blog;
 import com.lanqiao.circle.entity.Comments;
 import com.lanqiao.circle.entity.LikeKey;
+import com.lanqiao.circle.service.BlogService;
 import com.lanqiao.circle.service.CommentsService;
 import com.lanqiao.circle.service.LikeService;
 import com.lanqiao.circle.service.TokenService;
@@ -10,6 +12,7 @@ import com.lanqiao.circle.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +30,8 @@ public class BlogContoller {
     CommentsService commentsService;
     @Autowired
     LikeService likeService;
-
+    @Autowired
+    BlogService blogService;
     /**
      * 实现评论功能
      * @param httpServletRequest
@@ -36,7 +40,7 @@ public class BlogContoller {
      */
     @UserLoginToken
     @PostMapping("insertComment")
-    public Result insertComment(HttpServletRequest httpServletRequest, Comments comments){
+    public Result insertComment(HttpServletRequest httpServletRequest,Comments comments){
         int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
         comments.setUserId(userId);
         return commentsService.insertComment(comments);
@@ -54,5 +58,25 @@ public class BlogContoller {
         int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
         likeKey.setUserId(userId);
         return likeService.insertLike(likeKey);
+    }
+
+    /**
+     *
+     * @param httpServletRequest
+     * @param repostId
+     * @param forwardcontent
+     * @return
+     */
+    @UserLoginToken
+    @PostMapping("forwardBlog")
+    public Result forwardBlog(HttpServletRequest httpServletRequest, @RequestParam(name = "blogId") int repostId
+    ,@RequestParam(name = "forwardContent") String forwardcontent){
+        int userId = Integer.parseInt(tokenService.getUserId(httpServletRequest));
+        Blog blog  = new Blog();
+        blog.setUserId(userId);
+        blog.setIsRepost(1);
+        blog.setRepostId(repostId);
+        blog.setContent(forwardcontent);
+        return blogService.forwardBlog(blog);
     }
 }
