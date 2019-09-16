76eeebe6c98b599d535527d0258230b9b972aa47
C-H-Han <admin@honkerac.cn>
2019-09-16 18:43:07 +0800

    增加 判断是否关注接口

---
+++ src/main/java/com/lanqiao/circle/service/RelationShipService.java
@@ -0,0 +1,12 @@
+package com.lanqiao.circle.service;
+
+import com.lanqiao.circle.util.Result;
+
+/**
+ * @Author 王昊
+ * @Date 2019/9/16 6:28 下午
+ */
+public interface RelationShipService {
+    //确认是否关注
+    public Result ifConcern(int loginId,int userId);
+}
