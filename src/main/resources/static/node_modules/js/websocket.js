42f16a84c9b9389645e12533989b702b6e99b599
ohmyjason <719134834@qq.com>
2019-09-20 02:35:13 +0800

    借用一下120.79.30.62的websorck

--- src/main/resources/static/node_modules/js/websocket.js
+++ src/main/resources/static/node_modules/js/websocket.js
@@ -1,7 +1,7 @@
 var websocket = null;
 //判断当前浏览器是否支持WebSocket
 if ('WebSocket' in window) {
-    websocket = new WebSocket("ws://localhost:8080/websocket");
+    websocket = new WebSocket("ws://120.79.30.62:8080/websocket");
 } else {
     alert('Not support websocket');
 }
