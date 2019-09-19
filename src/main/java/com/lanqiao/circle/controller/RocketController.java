b675936b165385c4bc0656b5c357a5e30d10dd3e
iwaed7 <13909069948@163.com>
2019-09-19 10:18:19 +0800

    为了避免自己瞎搞然后炸死自己，在github上备份一下

---
+++ src/main/java/com/lanqiao/circle/controller/RocketController.java
@@ -0,0 +1,22 @@
+package com.lanqiao.circle.controller;
+
+import com.lanqiao.circle.service.impl.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 钱琦瑛
+ * @Date 2019/9/19 10:07
+ */
+@RestController
+@RequestMapping("/rocket")
+public class RocketController {
+    @Autowired
+    private ProducerService producer;
+
+    @RequestMapping("/push")
+    public String pushMsg(String msg) {
+        return producer.send("testTopic", "push", msg);
+    }
+}
